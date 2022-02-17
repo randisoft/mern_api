f811057eba12cdaa90e374b9026a17344dcaafb8
randisoft <90348300+randisoft@users.noreply.github.com>
2022-02-17 13:34:07 +0100

    Tasks controller and routes setup

--- backend/controllers/taskController.js
+++ backend/controllers/taskController.js
@@ -1,18 +1,24 @@
-const getTasks = (req, res) => {
+const asyncHandler = require('express-async-handler')
+
+const getTasks = asyncHandler(async(req, res) => {
     res.status(200).json({ message: 'Get tasks' })
-}
+})
 
-const createTask = (req, res) => {
+const createTask = asyncHandler(async(req, res) => {
+    if (!req.body.text) {
+        res.status(400)
+        throw new Error('Please add at text field')
+    }
     res.status(200).json({ message: 'Create a task' })
-}
+})
 
-const updateTask = (req, res) => {
+const updateTask = asyncHandler(async(req, res) => {
     res.status(200).json({ message: `Updated task ${req.params.id}` })
-}
+})
 
-const deleteTask = (req, res) => {
+const deleteTask = asyncHandler(async(req, res) => {
     res.status(200).json({ message: `Deleted task ${req.params.id}` })
-}
+})
 
 module.exports = {
     getTasks,
--- backend/routes/taskRoutes.js
+++ backend/routes/taskRoutes.js
@@ -2,12 +2,7 @@ const express = require('express')
 const router = express.Router()
 const { getTasks, createTask, updateTask, deleteTask } = require('../controllers/taskController')
 
-router.get('/', getTasks)
-
-router.post('/', createTask)
-
-router.put('/:id', updateTask)
-
-router.delete('/:id', deleteTask)
+router.route('/').get(getTasks).post(createTask)
+router.route('/:id').put(updateTask).delete(deleteTask)
 
 module.exports = router
